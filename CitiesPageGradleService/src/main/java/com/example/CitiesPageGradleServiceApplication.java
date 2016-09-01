33dc7dd28c0ed1f483f4051f1c46a7d0735fbfb2
Kostya Nikitin <kostya.nikitin@rp.ru>
2016-09-01 12:51:21 +0300

    Changing service CitiesPage from maven to gradle

--- CitiesPageGradleService/src/main/java/com/example/CitiesPageGradleServiceApplication.java
+++ CitiesPageGradleService/src/main/java/com/example/CitiesPageGradleServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Created by kostya.nikitin on 9/1/2016.
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class CitiesPageServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CitiesPageServiceApplication.class, args);
-	}
+public class CitiesPageGradleServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CitiesPageGradleServiceApplication.class, args);
+    }
 }
