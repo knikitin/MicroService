ce19175c131912abba745457d3c925ae04f4e365
Kostya Nikitin <kostya.nikitin@rp.ru>
2016-09-05 21:06:08 +0300

    Change the name of the service receiving data from the database

--- CitiesFromDBService/src/main/java/com/example/web/CitiesDBControllerAdvice.java
+++ CitiesFromDBService/src/main/java/com/example/web/CitiesDBControllerAdvice.java
@@ -5,15 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * Created by kostya.nikitin on 9/2/2016.
  */
 @RestControllerAdvice
-public class CitiesFromDBControllerAdvice {
-    final Logger slf4jLog = LoggerFactory.getLogger(CitiesFromDBControllerAdvice.class);
+public class CitiesDBControllerAdvice {
+    final Logger slf4jLog = LoggerFactory.getLogger(CitiesDBControllerAdvice.class);
 
     @ExceptionHandler(Exception.class)
     ResponseEntity<?> allErrorExceptionHandler(Throwable ex) {
