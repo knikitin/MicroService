e78c4fa32a6b7978d9cf8af7fac88d633fd00b1d
Kostya Nikitin <kostya.nikitin@rp.ru>
2016-09-07 18:12:02 +0300

    Delete  basic-error-controller from the files describing the API

--- CitiesUIService/src/main/java/com/example/util/SwaggerConfig.java
+++ CitiesUIService/src/main/java/com/example/util/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.example.util;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +29,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
                 .paths(PathSelectors.any()).build()
                 .useDefaultResponseMessages(false)
                 .globalResponseMessage(RequestMethod.GET,
